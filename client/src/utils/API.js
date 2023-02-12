5f8319cebbb462fc8e0bdc008d5e0e53e3c7587f
luluco92 <117111188+luluco92@users.noreply.github.com>
2023-02-12 15:11:26 -0500

    added files via upload

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-const search = async (query) =>
-  axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${query}&rating=pg`);
-
 const artsearch = async (query)=>
 axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=${query}`);
 
